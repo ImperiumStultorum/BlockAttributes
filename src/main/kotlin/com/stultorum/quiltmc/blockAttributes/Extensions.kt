22a5f406ccb9e067c7c3d264118c8839b5989154
CoolandonRS <49355953+CoolandonRS@users.noreply.github.com>
2024-05-01 14:35:10 -0600

    Delayed changes and finally forward events

--- src/main/kotlin/com/stultorum/quiltmc/blockAttributes/Extensions.kt
+++ src/main/kotlin/com/stultorum/quiltmc/blockAttributes/Extensions.kt
@@ -1,20 +1,29 @@
+@file:Suppress("unused")
+
 package com.stultorum.quiltmc.blockAttributes
 
 import com.stultorum.quiltmc.blockAttributes.mixinfs.IAttributeWorldChunk
+import com.stultorum.quiltmc.blockAttributes.mixinfs.IAttributeWorldChunk.AttributeEventType
+import com.stultorum.quiltmc.blockAttributes.nbt.DelayedAttributeChange
 import com.stultorum.quiltmc.blockAttributes.nbt.fromNbtCompound
 import com.stultorum.quiltmc.blockAttributes.nbt.getSerializer
 import com.stultorum.quiltmc.blockAttributes.nbt.toNbtCompound
+import com.stultorum.quiltmc.blunders.lang.Bool
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 import net.minecraft.world.chunk.Chunk
+import java.util.concurrent.CountDownLatch
 
 typealias BlockAttributes = Map<Identifier, NbtCompound>
 
+private data class DelayedChange(var lock: CountDownLatch, val change: DelayedAttributeChange)
+private val delayedChanges = HashMap<BlockPos, DelayedChange>()
 
 fun World.getBlockAttributesNbt(pos: BlockPos): BlockAttributes {
-    if (isOutOfHeightLimit(pos) || (!isClient && Thread.currentThread() != thread)) return HashMap()
+    if (!safeToQueryBlock(pos)) return HashMap()
+    delayedChanges[pos]?.lock?.await()
     return getWorldChunk(pos).getBlockAttributesNbt(pos)
 }
 
@@ -30,20 +39,48 @@ inline fun <reified T> World.setBlockAttribute(pos: BlockPos, id: Identifier, ob
  * Pass in null to remove.
  */
 fun World.setBlockAttributeNbt(pos: BlockPos, id: Identifier, obj: NbtCompound?) {
-    if (isOutOfHeightLimit(pos) || (!isClient && Thread.currentThread() != thread)) return
+    if (!safeToQueryBlock(pos)) return
+    delayedChanges[pos]?.lock?.await()
     getWorldChunk(pos).setBlockAttributeNbt(pos, id, obj)
 }
 
 fun World.setBlockAttributesNbt(pos: BlockPos, attributes: BlockAttributes) {
-    if (isOutOfHeightLimit(pos) || (!isClient && Thread.currentThread() != thread)) return
+    if (!safeToQueryBlock(pos)) return
+    delayedChanges[pos]?.lock?.await()
     getWorldChunk(pos).setBlockAttributesNbt(pos, attributes)
 }
 
-// TODO add forwarding for events
+fun World.requestAttributeChange(pos: BlockPos, id: Identifier, change: (NbtCompound) -> NbtCompound) {
+    if (!safeToQueryBlock(pos)) return
+    if (!delayedChanges.containsKey(pos)) {
+        delayedChanges[pos] = DelayedChange(CountDownLatch(1), DelayedAttributeChange(pos))
+    }
+    val delayedChange = delayedChanges[pos]!!
+    if (delayedChange.lock.count == 0L) delayedChange.lock = CountDownLatch(1)
+    delayedChange.change.request(id, change)
+}
+
+fun World.applyAttributeChange(pos: BlockPos) {
+    if (!safeToQueryBlock(pos)) return
+    delayedChanges[pos]!!.change.apply(this)
+    delayedChanges[pos]!!.lock.countDown()
+}
+
+fun World.addAttributeListener(type: AttributeEventType, pos: BlockPos, callback: () -> Unit) {
+    if (!safeToQueryBlock(pos)) return
+    getWorldChunk(pos).addAttributeListener(type, pos, callback)
+}
+
+fun World.removeAttributeListener(type: AttributeEventType, pos: BlockPos, callback: () -> Unit) {
+    if (!safeToQueryBlock(pos)) return
+    getWorldChunk(pos).removeAttributeListener(type, callback)
+}
+
+private fun World.safeToQueryBlock(pos: BlockPos): Bool = isOutOfHeightLimit(pos) || (!isClient && Thread.currentThread() != thread)
 
 // Chunk -> IAttributeWorldChunk util
 @Suppress("NOTHING_TO_INLINE") // Literally just done because this simple of a function not being inlined makes less sense to me then the opposite.
-inline fun Chunk.asAttributeChunk() = (this as IAttributeWorldChunk)
+internal inline fun Chunk.asAttributeChunk() = (this as IAttributeWorldChunk)
 inline fun <reified T: Any> Chunk.setBlockAttribute(pos: BlockPos, id: Identifier, obj: T?) = setBlockAttributeNbt(pos, id, if (obj == null) null else getSerializer<T>().serialize(obj))
 inline fun <reified T: Any> Chunk.getBlockAttribute(pos: BlockPos, id: Identifier): T? {
     val nbt = getBlockAttributeNbt(pos, id) ?: return null
@@ -57,3 +94,9 @@ fun Chunk.setBlockAttributesNbt(pos: BlockPos, attributes: BlockAttributes?): Un
 fun Chunk.setBlockAttributeNbt(pos: BlockPos, id: Identifier, nbt: NbtCompound?): Unit = if (nbt == null) removeBlockAttribute(pos, id) else asAttributeChunk().setBlockAttribute(pos, id, nbt)
 fun Chunk.removeBlockAttribute(pos: BlockPos, id: Identifier): Unit = asAttributeChunk().removeBlockAttribute(pos, id)
 fun Chunk.clearBlockAttributes(pos: BlockPos): Unit = asAttributeChunk().clearBlockAttributes(pos)
+fun Chunk.addAttributeListener(type: AttributeEventType, pos: BlockPos, callback: () -> Unit) = asAttributeChunk().addAttributeListener(type, pos, callback)
+fun Chunk.addAttributeListener(type: AttributeEventType, callback: (BlockPos) -> Unit) = asAttributeChunk().addAttributeListener(type, callback)
+fun Chunk.addAttributeListener(type: AttributeEventType, condition: (BlockPos) -> Bool, callback: () -> Unit) = addAttributeListener(type, condition) { _ -> callback() }
+fun Chunk.addAttributeListener(type: AttributeEventType, condition: (BlockPos) -> Bool, callback: (BlockPos) -> Unit) = asAttributeChunk().addAttributeListener(type, condition, callback)
+fun Chunk.removeAttributeListener(type: AttributeEventType, callback: () -> Unit) = asAttributeChunk().removeAttributeListener(type, callback)
+fun Chunk.removeAttributeListener(type: AttributeEventType, callback: (BlockPos) -> Unit) = asAttributeChunk().removeAttributeListener(type, callback)
